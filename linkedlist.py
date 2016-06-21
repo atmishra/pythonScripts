f2f2623e76dd960149345129987b74668dc7a33e
Atul <at.mishra007@gmail.com>
2016-06-21 11:05:46 +0530

    Added Linked List Implementation

---
+++ linkedlist.py
@@ -0,0 +1,78 @@
+class Node():
+	def __init__(self,data):
+		self.data=data
+		self.next=None
+		self.index=0
+
+	def getData(self):
+		return self.data
+
+	def getNext(self):
+		return self.next
+
+	def getIndex():
+		return self.index
+
+class LinkedList(Node):
+	head = None
+	count = 0
+	def __init__(self):
+		self.head = None
+		self.count = 0
+
+	def insertEnd(self,data):
+		
+		node = Node(data)
+		self.count+=1
+		node.index = self.count
+
+		if self.head == None:
+			self.head = node
+		else:
+			temp = self.iterate()
+			temp.next = node
+
+	def insertFront(self,data):
+		
+		node = Node(data)
+		self.count+=1
+		node.index = self.count
+
+		if self.head == None:
+			self.head = node
+		else:
+			node.next = self.head
+			self.head = node
+
+	def iterate(self):
+		temp = self.head
+		while temp.next:
+			temp = temp.next
+		return temp
+
+	def printList(self):
+		temp = self.head
+		for i in range(self.count):
+			
+			print(temp.data)
+			temp=temp.next
+
+if __name__ == "__main__":
+	ll = LinkedList()
+	ll.insertEnd(10)
+	ll.insertEnd(20)
+	ll.insertEnd(40)
+	ll.insertEnd(50)
+	ll.insertEnd(10)
+	ll.insertEnd(30)
+	
+	ll.insertFront(1)
+	ll.insertFront(2)
+	ll.insertFront(3)
+	ll.printList()
+	print("Count: ",ll.count)
+
+
+
+
+
